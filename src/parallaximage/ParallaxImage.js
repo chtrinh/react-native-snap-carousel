0aed9879daee2a8095f1962a7e15ddcb5543ff51
chtrinh <chris.chtrinh@gmail.com>
2018-03-14 15:58:39 -0700

    Allow passing wrapped Animated image components as props

--- src/parallaximage/ParallaxImage.js
+++ src/parallaximage/ParallaxImage.js
@@ -1,12 +1,19 @@
 // Parallax effect inspired by https://github.com/oblador/react-native-parallax/
 
-import React, { Component } from 'react';
-import { View, ViewPropTypes, Image, Animated, Easing, ActivityIndicator, findNodeHandle } from 'react-native';
-import PropTypes from 'prop-types';
-import styles from './ParallaxImage.style';
+import React, { Component } from "react";
+import {
+    View,
+    ViewPropTypes,
+    Image,
+    Animated,
+    Easing,
+    ActivityIndicator,
+    findNodeHandle
+} from "react-native";
+import PropTypes from "prop-types";
+import styles from "./ParallaxImage.style";
 
 export default class ParallaxImage extends Component {
-
     static propTypes = {
         ...Image.propTypes,
         carouselRef: PropTypes.object, // passed from <Carousel />
@@ -16,7 +23,9 @@ export default class ParallaxImage extends Component {
         sliderHeight: PropTypes.number, // passed from <Carousel />
         sliderWidth: PropTypes.number, // passed from <Carousel />
         vertical: PropTypes.bool, // passed from <Carousel />
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        containerStyle: ViewPropTypes
+            ? ViewPropTypes.style
+            : View.propTypes.style,
         dimensions: PropTypes.shape({
             width: PropTypes.number,
             height: PropTypes.number
@@ -32,10 +41,10 @@ export default class ParallaxImage extends Component {
         fadeDuration: 500,
         parallaxFactor: 0.3,
         showSpinner: true,
-        spinnerColor: 'rgba(0, 0, 0, 0.4)'
-    }
+        spinnerColor: "rgba(0, 0, 0, 0.4)"
+    };
 
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
             offset: 0,
@@ -48,11 +57,11 @@ export default class ParallaxImage extends Component {
         this._onError = this._onError.bind(this);
     }
 
-    setNativeProps (nativeProps) {
+    setNativeProps(nativeProps) {
         this._container.setNativeProps(nativeProps);
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this._mounted = true;
 
         setTimeout(() => {
@@ -60,11 +69,11 @@ export default class ParallaxImage extends Component {
         }, 0);
     }
 
-    componentWillUnmount () {
+    componentWillUnmount() {
         this._mounted = false;
     }
 
-    _measureLayout () {
+    _measureLayout() {
         if (this._container) {
             const {
                 dimensions,
@@ -80,18 +89,20 @@ export default class ParallaxImage extends Component {
                 this._container.measureLayout(
                     findNodeHandle(carouselRef),
                     (x, y, width, height, pageX, pageY) => {
-                        const offset = vertical ?
-                            y - ((sliderHeight - itemHeight) / 2) :
-                            x - ((sliderWidth - itemWidth) / 2);
+                        const offset = vertical
+                            ? y - (sliderHeight - itemHeight) / 2
+                            : x - (sliderWidth - itemWidth) / 2;
 
                         this.setState({
                             offset: offset,
-                            width: dimensions && dimensions.width ?
-                                dimensions.width :
-                                Math.ceil(width),
-                            height: dimensions && dimensions.height ?
-                                dimensions.height :
-                                Math.ceil(height)
+                            width:
+                                dimensions && dimensions.width
+                                    ? dimensions.width
+                                    : Math.ceil(width),
+                            height:
+                                dimensions && dimensions.height
+                                    ? dimensions.height
+                                    : Math.ceil(height)
                         });
                     }
                 );
@@ -99,7 +110,7 @@ export default class ParallaxImage extends Component {
         }
     }
 
-    _onLoad (event) {
+    _onLoad(event) {
         const { animOpacity } = this.state;
         const { fadeDuration, onLoad } = this.props;
 
@@ -125,7 +136,7 @@ export default class ParallaxImage extends Component {
     }
 
     // If arg is missing from method signature, it just won't be called
-    _onError (event) {
+    _onError(event) {
         const { onError } = this.props;
 
         this.setState({ status: 4 });
@@ -135,7 +146,7 @@ export default class ParallaxImage extends Component {
         }
     }
 
-    get image () {
+    get image() {
         const { status, animOpacity, offset, width, height } = this.state;
         const {
             scrollPosition,
@@ -149,66 +160,92 @@ export default class ParallaxImage extends Component {
         } = this.props;
 
         const parallaxPadding = (vertical ? height : width) * parallaxFactor;
-        const requiredStyles = { position: 'relative' };
+        const requiredStyles = { position: "relative" };
         const dynamicStyles = {
             width: vertical ? width : width + parallaxPadding * 2,
             height: vertical ? height + parallaxPadding * 2 : height,
             opacity: animOpacity,
-            transform: scrollPosition ? [
-                {
-                    translateX: !vertical ? scrollPosition.interpolate({
-                        inputRange: [offset - sliderWidth, offset + sliderWidth],
-                        outputRange: [-parallaxPadding, parallaxPadding],
-                        extrapolate: 'clamp'
-                    }) : 0
-                },
-                {
-                    translateY: vertical ? scrollPosition.interpolate({
-                        inputRange: [offset - sliderHeight, offset + sliderHeight],
-                        outputRange: [-parallaxPadding, parallaxPadding],
-                        extrapolate: 'clamp'
-                    }) : 0
-                }
-            ] : []
+            transform: scrollPosition
+                ? [
+                      {
+                          translateX: !vertical
+                              ? scrollPosition.interpolate({
+                                    inputRange: [
+                                        offset - sliderWidth,
+                                        offset + sliderWidth
+                                    ],
+                                    outputRange: [
+                                        -parallaxPadding,
+                                        parallaxPadding
+                                    ],
+                                    extrapolate: "clamp"
+                                })
+                              : 0
+                      },
+                      {
+                          translateY: vertical
+                              ? scrollPosition.interpolate({
+                                    inputRange: [
+                                        offset - sliderHeight,
+                                        offset + sliderHeight
+                                    ],
+                                    outputRange: [
+                                        -parallaxPadding,
+                                        parallaxPadding
+                                    ],
+                                    extrapolate: "clamp"
+                                })
+                              : 0
+                      }
+                  ]
+                : []
         };
 
-        return (
-            <Animated.Image
-              {...other}
-              style={[styles.image, style, requiredStyles, dynamicStyles]}
-              onLoad={this._onLoad}
-              onError={status !== 3 ? this._onError : undefined} // prevent infinite-loop bug
-            />
-        );
+        // <Animated.Image
+        // {...other}
+        // style={[styles.image, style, requiredStyles, dynamicStyles]}
+        // onLoad={this._onLoad}
+        // onError={status !== 3 ? this._onError : undefined} // prevent infinite-loop bug
+        //>;
+        return React.cloneElement(this.props.imageComponent || Animated.Image, {
+            ...other,
+            style: [styles.image, style, requiredStyles, dynamicStyles],
+            onLoad: this._onLoad,
+            onError: status !== 3 ? this._onError : undefined
+        });
     }
 
-    get spinner () {
+    get spinner() {
         const { status } = this.state;
         const { showSpinner, spinnerColor } = this.props;
 
         return status === 1 && showSpinner ? (
             <View style={styles.loaderContainer}>
                 <ActivityIndicator
-                  size={'small'}
-                  color={spinnerColor}
-                  animating={true}
+                    size={"small"}
+                    color={spinnerColor}
+                    animating={true}
                 />
             </View>
-        ) : false;
+        ) : (
+            false
+        );
     }
 
-    render () {
+    render() {
         const { containerStyle } = this.props;
 
         return (
             <View
-              ref={(c) => { this._container = c; }}
-              pointerEvents={'none'}
-              style={[containerStyle, styles.container]}
-              onLayout={this._measureLayout}
+                ref={c => {
+                    this._container = c;
+                }}
+                pointerEvents={"none"}
+                style={[containerStyle, styles.container]}
+                onLayout={this._measureLayout}
             >
-                { this.image }
-                { this.spinner }
+                {this.image}
+                {this.spinner}
             </View>
         );
     }
